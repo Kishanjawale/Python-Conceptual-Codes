b106cedd608089945c6367bc15292f1a58de34ba
KISHAN JAWALE <kishanjawale10@gmail.com>
2022-09-18 22:47:23 +0530

    Update Datatypes.py

--- Datatypes.py
+++ Datatypes.py
@@ -1,8 +1,16 @@
+
+#Integer
 a= 5
 print("type of A:", type(a))
 
+#Float
 b= 5.5
 print("Type of B: ", type(b))
 
+#Complex
 c= 2 + 4j
 print("Type of C: ",type(c))
+
+#string
+str =  "Welcome to Marvellous"
+print("Type of str: ",type(str))
