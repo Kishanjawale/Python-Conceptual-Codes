dc70fc2547e21ba7b55ce2d7a42bac1adf9bc1fb
KISHAN JAWALE <kishanjawale10@gmail.com>
2022-09-19 10:19:06 +0530

    Add files via upload

---
+++ UserInput.py
@@ -0,0 +1,12 @@
+print("This application demonstrate how to take input from user")
+
+
+print("Enter first Number")
+x=input()
+
+print("Enter second Number")
+y=input()
+z = 0
+z = int (x) +int(y) 
+
+print(z)
