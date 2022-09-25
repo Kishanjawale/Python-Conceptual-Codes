7f5020553eba25f4d2449a261041398eb01479fa
KISHAN JAWALE <kishanjawale10@gmail.com>
2022-09-25 23:51:11 +0530

    Update MarvellousModule.py

--- Module Creation/MarvellousModule.py
+++ Module Creation/MarvellousModule.py
@@ -1,4 +1,18 @@
 def Addition(iNo1,iNo2):   #User Defined Function
     ans= iNo1+iNo2
     return  ans
+def Substraction(iNo1,iNo2):    #User Defined Function
+    ans = iNo1 - iNo2
+    return ans
+
+def Division(iNo1,iNo2):      #User Defined Function
+    ans = iNo1 / iNo2
+    return ans
+
+def Multiplication(iNo1,iNo2):     #User Defined Function
+    ans = iNo1 * iNo2
+    return ans
+
+
+
 
