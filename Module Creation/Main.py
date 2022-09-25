68c5145a034d766c9d16b5252e181d5762de7c30
KISHAN JAWALE <kishanjawale10@gmail.com>
2022-09-25 23:51:54 +0530

    Update Main.py

--- Module Creation/Main.py
+++ Module Creation/Main.py
@@ -10,9 +10,19 @@ def main():
     print("Enter Second Number")
     no2=int(input())
 
-    iRet = MarvellousModule.Addition(no1,no2)
 
+    iRet = MarvellousModule.Addition(no1,no2)
     print("Addition is :",iRet)
+
+    iRet = MarvellousModule.Substraction(no1,no2)
+    print("Substraction is :",iRet)
+
+    iRet = MarvellousModule.Multiplication(no1, no2)
+    print("Multiplication is :", iRet)
+
+    iRet = MarvellousModule.Division(no1, no2)
+    print("Division is :", iRet)
+
 
 if __name__ == "__main__":      #starter
     main()
