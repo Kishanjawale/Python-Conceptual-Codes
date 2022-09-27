67c2e470db409abc4cd2441055dadfb6e8c4f0d6
KISHAN JAWALE <kishanjawale10@gmail.com>
2022-09-27 20:38:31 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,10 @@
+#This application demonstarate the concept of List
+mylist = ["BMW","Toyota","RR","Porche"]
+
+print(mylist) # this function will print the list on screen
+
+print(type(mylist)) #this function will  display type of mylist.
+
+mylist1 = ["11","Banana","21",True,"Apple",False,"51","Cherry"] #list can contain integer as well as boolean and string
+
+print(mylist1)
